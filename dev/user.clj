d25847711f73a120c693981bad784363b663959b
Milt Reder <milt@anestuary.com>
2016-02-16 10:26:30 -0500

    added examples for nippy/fress

--- dev/user.clj
+++ dev/user.clj
@@ -1,18 +1,56 @@
 (ns user
-  (:require [reek.core :as reek]))
+  (:require [reek.core :as reek]
+            [clojure.data.fressian :as fress]
+            [taoensso.nippy :as nip]))
 
+(defn store-fress [conn b k clj-data idx-map]
+  (reek/store conn b k (fress/write clj-data) :byte-buffer "application/fressian" idx-map))
 
-(comment (def conn
-           (reek/connect "127.0.0.1" 8087))
+(defn fetch-fress [conn b k]
+  (fress/read (:value (reek/fetch conn b k :byte-buffer))))
 
-(reek/store conn "test-bucket" "test-key" "foo" :string "application/foo" {})
-(reek/store conn "test-bucket" "test-key2" "foo" :string "application/foo" {:foo "bar"})
-(reek/fetch conn  "test-bucket" "test-key2" :string)
+(defn store-nippy [conn b k clj-data idx-map]
+  (reek/store conn b k (nip/freeze clj-data) :byte-array "application/fressian" idx-map))
 
-(reek/delete conn "test-bucket" "test-key2")
+(defn fetch-nippy [conn b k]
+  (nip/thaw (:value (reek/fetch conn b k :byte-array))))
 
-(reek/query-eq conn "test-bucket" :foo "baz")
-(reek/query-range conn "test-bucket" :foo "bar" "baz")
+(comment
 
-(reek/shutdown conn)
-)
+  (def conn
+    (reek/connect "127.0.0.1" 8087))
+
+  (reek/store conn "test-bucket" "a-string" "foo" :string "text/plain" {})
+  (reek/fetch conn  "test-bucket" "a-string" :string)
+
+  (reek/delete conn "test-bucket" "a-string")
+
+  (reek/store conn "test-bucket"
+              "a-string-with-an-index" "quxx"
+              :string "text/plain" {:foo "bar"})
+  (reek/store conn "test-bucket"
+              "another-string-with-an-index" "wizz" :string "text/plain" {:foo "bar"})
+  (reek/store conn "test-bucket"
+              "a-third-string-with-a-different-index" "wazz" :string "text/plain" {:foo "baz"})
+
+  (reek/query-eq conn "test-bucket" :foo "bar")
+  (reek/query-eq conn "test-bucket" :foo "baz")
+
+  (reek/query-range conn "test-bucket" :foo "bar" "baz")
+
+  (reek/delete conn "test-bucket" "a-string-with-an-index")
+  (reek/delete conn "test-bucket" "another-string-with-an-index")
+  (reek/delete conn "test-bucket" "a-third-string-with-a-different-index")
+
+  (store-fress conn "test-bucket" "a-fressian-value" {:some {:fressian ["stuff"]}} {})
+  (fetch-fress conn "test-bucket" "a-fressian-value")
+
+  (reek/delete conn "test-bucket" "a-fressian-value")
+
+  (store-nippy conn "test-bucket" "a-nippy-value" {:some {:nippy ["stuff"]}} {})
+  (fetch-nippy conn "test-bucket" "a-nippy-value")
+
+  (reek/delete conn "test-bucket" "a-nippy-value")
+
+  (reek/shutdown conn)
+  )
--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
 (defproject com.yetanalytics/reek "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Simple Riak k/v library"
+  :url "https://github.com/yetanalytics/reek"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.yetanalytics/kria "0.2.0-SNAPSHOT"]]
+                 [com.yetanalytics/kria "0.2.0-SNAPSHOT"]
+                 [prismatic/schema "1.0.5"]]
   :profiles
-  {:dev {:source-paths ["dev"]}})
+  {:dev {:source-paths ["dev"]
+         :dependencies [[org.clojure/data.fressian "0.2.1"]
+                        [com.taoensso/nippy "2.10.0"]]}})
